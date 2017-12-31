fd9064d69dbe2b958b21d75d6936f8c38cd88b5b
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-12-31 17:10:47 -0600

    Added Core Data

--- Goal Post Again/Controller/GoalVC.swift
+++ Goal Post Again/Controller/GoalVC.swift
@@ -14,10 +14,28 @@ class GoalVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
     }
     
     @IBAction func addGoalButtonWasPressed(_ sender: Any) {
         print("Button was Pressed")
+    }
+}
+
+extension GoalVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat pizza forever", type: .shortTerm, goalProgressAmount: 100)
+        return cell
     }
 }
---
+++ Goal Post Again/Enums/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  Goal Post Again
+//
+//  Created by Chris McDonald on 12/31/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+    
+}
